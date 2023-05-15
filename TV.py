4c18b3a0018a14be695ce10c7b26ba9a5cafa895
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-15 14:28:47 +0700

    The following are created: File for the class tv and testTV, class Tv, and the parameterized constructor with the attributes of the tv indicated in the UML

---
+++ TV.py
@@ -0,0 +1,8 @@
+#create the class
+class Tv:
+    #create a parameterized constructor
+    def __init__(self,channel,volume_level, on_off):
+        #attributes of the TV
+        self.channel = channel
+        self.volume_level = volume_level
+        self.on_off = on_off
