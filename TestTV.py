4a5990f57a88708e778a4d0d2378322c9f70543e
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-15 22:43:10 +0700

    EXTRA: Design

--- TestTV.py
+++ TestTV.py
@@ -1,13 +1,21 @@
 from ClassTv import Tv
+from colorama import Fore, Style
 
-print("For Television 1: \n")
+#FOR TELEVISION
+print(Fore.YELLOW + "\nFor Telivision 1:")
+print(Fore.BLUE + "=" * 105 + Fore.WHITE + Style.NORMAL)
 #construct a TV Object
 television_1 = Tv()
 #access method inside class
-print("Tv1's channel is " + str(television_1.get_channel()) + " and volume level is " + str(television_1.get_volume()))
+print(Fore.BLUE + "=" * 105)
+print(Fore.WHITE + "Tv1's channel is " + str(television_1.get_channel()) + " and volume level is " + str(television_1.get_volume()))
 
-print("\nFor Television 2: \n")
+
+#FOR TELEVISION 2
+print(Fore.YELLOW + "\n\nFor Telivision 2:")
+print(Fore.BLUE + "=" * 105 + Fore.WHITE + Style.NORMAL)
 #construct a TV Object
 television_2 = Tv()
 #access method inside class
-print("Tv1's channel is " + str(television_2.get_channel()) + " and volume level is " + str(television_.get_volume()))
+print(Fore.BLUE + "=" * 105)
+print(Fore.WHITE +"Tv2's channel is " + str(television_2.get_channel()) + " and volume level is " + str(television_2.get_volume()))
