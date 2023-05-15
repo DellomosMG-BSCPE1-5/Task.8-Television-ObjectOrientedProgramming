64ff562d1cc6d0dcda3df5c35d720d546773cee9
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-15 20:22:30 +0700

    ClassTv.py ready to test into TestTV.py file

--- ClassTv.py
+++ ClassTv.py
@@ -5,50 +5,46 @@ class Tv:
         #attributes of the TV
         self.channel = int(input("Enter your desired channel: "))
         self.volume_level = int(input("Enter your desired volume level: "))
- 
-
+        self.on = "On"
 
     #methods
     def tv(self):
-        self.tv_number = int(input("What is the number of your Tv? Enter the number that corresponds to your Tv: "))
-        return(self.tv_number)
+        self.television = (input("Create your own television. Type 'tv', underscore(_), and the number you want. Example: 'tv_1':  "))
+        return(self.television)
     
     def turn_on(self):
-        self.on_off = True
+        self.on = True
 
     def turn_off(self):
-        self.on_off = False
+        self.on = False
     
     def get_channel(self):
-        channel = int(input("Enter your desired channel: "))
+        return(self.channel)
+    
+    def set_channel(self, channel):
         self.channel = channel
-    
-    def set_channel(self,channel):
-        print("Your desired channel is" + str(channel))
-    
+
     def get_volume(self):
-        volume_level = int(input("Enter your desired volume level: "))
+        return(self.volume_level)
+    
+    def set_volume(self, volume_level):
         self.volume_level = volume_level
-        return(self.volume_level)
-    
-    def set_volume(self):
-        return(self.volume_level)
-    
+
     def channel_up(self):
         channel_up = self.channel + 1
-        print (channel_up)
+        print ("Channel: " + str(channel_up))
     
     def channel_down(self):
         channel_down = self.channel - 1
-        print (channel_down)
+        print ("Channel: " + str(channel_down))
 
     def volume_up(self):
         volume_up = self.volume_level + 1
-        print (volume_up)
+        print ("Channel: " + str(volume_up))
     
     def volume_down(self):
         volume_down = self.volume_level - 1
-        print (volume_down)
+        print ("Channel: " + str(volume_down))
 
 
 
--- TestTV.py
+++ TestTV.py
@@ -1,7 +1,2 @@
 from ClassTv import Tv
 
-tv_1 = Tv()
-print("tv" + str(tv_1.tv()) + "'s channel is", (tv_1.set_volume()))
-
-
-
