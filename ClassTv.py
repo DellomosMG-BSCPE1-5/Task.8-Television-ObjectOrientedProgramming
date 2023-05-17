4a360726016ddc692c986726d44fe776531db74e
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-17 11:12:06 +0700

    Changed the logic of def turn_on() and def turn_off(). I change also the get_channel and get_volume methods para mas maunang lumabas sa terminal yung turn_on and turn_off methods kesa sa questions about channel and volume.

--- ClassTv.py
+++ ClassTv.py
@@ -2,32 +2,42 @@
 #create the class
 class Tv:
     #create a parameterized constructor
-    def __init__(self):
+    def __init__(self, channel = "", volume_level = ""):
         #attributes of the TV
-        self.channel = int(input("Enter your desired channel (1-120 only): "))
-        self.volume_level = int(input("Enter your desired volume level (1-7 only): "))
         self.on = "ON"
+        self.channel = channel
+        self.volume_level = volume_level
 
     #methods
     def turn_on(self):
         if self.on == "ON":
-            print ("Turn On the TV")
+            print("Your TV is already ON.")
+        else:
+            print("Turning On the TV...")
+            self.on = "ON"     
 
     def turn_off(self):
-        if self.on != "ON":
-            print ("Turn Off the TV")
-    
+        if self.on == "OFF":
+            print("Your TV is already OFF.")
+        else:
+            print("Turning Off the TV...")
+            self.on = "OFF"
+
     def get_channel(self):
+        self.channel = int(input("Enter your desired channel (1-120 only): "))
         return(self.channel)
     
     def set_channel(self, channel):
         self.channel = channel
+        print("You have set the channel into " + str(self.volume_level))
 
     def get_volume(self):
+        self.volume_level = int(input("Enter your desired volume level (1-7 only): "))
         return(self.volume_level)
     
     def set_volume(self, volume_level):
         self.volume_level = volume_level
+        print("You have set the volume into " + str(self.volume_level))
 
     def channel_up(self):
         channel_up = self.channel + 1
--- TestTV.py
+++ TestTV.py
@@ -1,20 +1,22 @@
 from ClassTv import Tv
 from colorama import Fore, Style
 
+
 #FOR TELEVISION 1
 print(Fore.YELLOW + "\n\nFor Telivision 1: \n" + Fore.BLUE + "=" * 105 + Fore.WHITE + Style.NORMAL)
+
 #construct a TV Object
 television_1 = Tv()
-print(Fore.BLUE + "=" * 105)
 #access method inside class
-print(Fore.WHITE + "Tv1's channel is " + str(television_1.get_channel()) + " and volume level is " + str(television_1.get_volume()))
-
+print(Fore.YELLOW + "\nTv1's channel is " + str(television_1.get_channel()) + " and volume level is " + str(television_1.get_volume()))
+print(Fore.BLUE + "=" * 105)
 
 
 #FOR TELEVISION 2
 print(Fore.YELLOW + "\n\nFor Telivision 2: \n" + Fore.BLUE + "=" * 105 + Fore.WHITE + Style.NORMAL)
+
 #construct a TV Object
 television_2 = Tv()
-print(Fore.BLUE + "=" * 105)
 #access method inside class
-print(Fore.WHITE +"Tv2's channel is " + str(television_2.get_channel()) + " and volume level is " + str(television_2.get_volume()))
+print(Fore.YELLOW + "\nTv2's channel is " + str(television_2.get_channel()) + " and volume level is " + str(television_2.get_volume()))
+print(Fore.BLUE + "=" * 105 + "\n")
