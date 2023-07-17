b8ff39820cdb92d7f82c252ac2798b7d0e4cca70
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-07-17 20:22:18 +0700

    the code has been modified

--- ClassTv.py
+++ ClassTv.py
@@ -22,23 +22,19 @@ class Tv:
         else:
             print("Turning Off the TV...")
             self.on = "OFF"
+    
+    def set_channel(self):
+        self.channel = int(input("Enter your desired channel (1-120 only): "))
 
     def get_channel(self):
-        self.channel = int(input("Enter your desired channel (1-120 only): "))
         return(self.channel)
-    
-    def set_channel(self, channel):
-        self.channel = channel
-        print("You have set the channel into " + str(self.volume_level))
+
+    def set_volume(self):
+        self.volume_level = int(input("Enter your desired volume level (1-7 only): "))
 
     def get_volume(self):
-        self.volume_level = int(input("Enter your desired volume level (1-7 only): "))
         return(self.volume_level)
     
-    def set_volume(self, volume_level):
-        self.volume_level = volume_level
-        print("You have set the volume into " + str(self.volume_level))
-
     def channel_up(self):
         channel_up = self.channel + 1
         print ("Channel: " + str(channel_up))
--- TestTV.py
+++ TestTV.py
@@ -8,6 +8,8 @@ print(Fore.YELLOW + "\n\nFor Telivision 1: \n" + Fore.BLUE + "=" * 105 + Fore.WH
 #construct a TV Object
 television_1 = Tv()
 #access method inside class
+television_1.set_channel()
+television_1.set_volume()
 print(Fore.YELLOW + "\nTv1's channel is " + str(television_1.get_channel()) + " and volume level is " + str(television_1.get_volume()))
 print(Fore.BLUE + "=" * 105)
 
@@ -18,5 +20,7 @@ print(Fore.YELLOW + "\n\nFor Telivision 2: \n" + Fore.BLUE + "=" * 105 + Fore.WH
 #construct a TV Object
 television_2 = Tv()
 #access method inside class
+television_2.set_channel()
+television_2.set_volume()
 print(Fore.YELLOW + "\nTv2's channel is " + str(television_2.get_channel()) + " and volume level is " + str(television_2.get_volume()))
 print(Fore.BLUE + "=" * 105 + "\n")
